86508a19d1d73fa81cdf7f5281fc95d3b7a674e5
Christoph Menzel <christoph.menzel@methodpark.de>
2021-10-12 14:15:19 +0200

    Fix findings from pub.dev
    Add a longer description to the pubspec.yaml
    Formatted correctly
    Update the formatter and vscode settings for the formatter

--- tool/check-dart-fmt.sh
+++ tool/check-dart-fmt.sh
@@ -4,18 +4,16 @@ set -e
 
 cd "$(dirname "$0")"
 
-lineLength=100
-
 # lib
 libFolder="../lib"
 
-command="dart format -l $lineLength -o none --set-exit-if-changed $libFolder"
+command="dart format -o none --set-exit-if-changed $libFolder"
 
 $command
 
 #test
 testFolder="../test"
 
-command="dart format -l $lineLength -o none --set-exit-if-changed $testFolder"
+command="dart format -o none --set-exit-if-changed $testFolder"
 
 $command
